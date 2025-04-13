84ed633927caeeb09134496decbfd26b76b8c973
Jessica Curney <jcurney@umich.edu>
2025-04-13 07:11:44 +0000

    treatcounter

--- src/components/TreatCounter.js
+++ src/components/TreatCounter.js
@@ -3,7 +3,8 @@ import React from 'react';
 function TreatCounter({ treats, incrementTreats }) {
   return (
     <div>
-      <h1>Feed Me 3 Treats to See My Friends! {treats}</h1>
+      <h1>Feed Me 3 Treats to See My Friends!</h1>
+      <p style={{ fontSize: '1.5em' }}>Treats Given: {treats}</p>
       <button onClick={incrementTreats} style={{ fontSize: '10em' }}>🐶</button>
     </div>
   );
