84b596c59786118b036821d2eb1e41191f859f16
Jessica Curney <jcurney@umich.edu>
2025-04-19 16:24:12 +0000

    remove css

--- src/components/BreedPicker.js
+++ src/components/BreedPicker.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './BreedPicker.css';
 
 function BreedPicker({ setSelectedBreed, selectedBreed, breedData }) {
   const [selectedCategory, setSelectedCategory] = useState('');
