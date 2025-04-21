22670b25c97a34ca702d8918f578c25c7832d8ae
Jessica Curney <jcurney@umich.edu>
2025-04-21 02:34:49 +0000

    breed JSDoc

--- src/components/BreedPicker.js
+++ src/components/BreedPicker.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react';
 
+
+/**
+ * This component allows the user to select a category of dog breeds sizes, big or little, and then pick a specific breed
+ * from that category. It displays specific breed information once a breed is selected.
+ *
+ * @component
+ * @param {Object} props - the component props
+ * @param {Function} props.setSelectedBreed - function to set the currently selected breed
+ * @param {string} props.selectedBreed - currently selected breed
+ * @param {Object} props.breedData - an object that contains breed information categorized by type and each category should
+ * contain an array of breed objects with properties such as `name`, `size`, and `temperament` created in App.js.
+ * @returns {JSX.Element} the rendered component
+ */
+
 function BreedPicker({ setSelectedBreed, selectedBreed, breedData }) {
   const [selectedCategory, setSelectedCategory] = useState('');
   const [selectedBreedData, setSelectedBreedData] = useState(null);
